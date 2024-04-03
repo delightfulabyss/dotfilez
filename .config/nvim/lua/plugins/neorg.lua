e87efc656ec892a5e7ea38d5db01fd2086d49696
Matthew Watman <mattwatman@gmail.com>
2024-04-03 13:12:12 -0400

    Update neorg to latest version

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -3,5 +3,25 @@ return {
 	dependencies = { "luarocks.nvim" },
 	lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 	version = "*", -- Pin Neorg to the latest stable release
-	config = true,
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},  -- Loads default behaviour
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							personal = "~/Documents/Sync/notes/personal",
+							work = "~/Documents/Sync/notes/work",
+						},
+					},
+				}, -- Manages Neorg workspaces
+				["core.completion"] = {
+					config = {
+						engine = "nvim-cmp",
+					},
+				},
+			},
+		})
+	end,
 }
