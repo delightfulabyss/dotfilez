2ae6c1c31ec4d4d3396edee24f2ef647321e2397
Matthew Watman <mattwatman@gmail.com>
2024-06-06 10:26:58 -0400

    update filename and add function for submodule removal

--- aliases_functions.zsh
+++ aliases_functions.zsh
@@ -1,5 +1,9 @@
 # aliases
 alias v="nvim"
+alias r="reboot"
+alias o="poweroff"
+alias h="sudo systemctl hibernate"
+alias rz="source $HOME/.zshrc && echo '.zshrc reloaded!'"
 alias d='/usr/bin/git --git-dir=/home/matthew/.dotfilez/ --work-tree=/home/matthew'
 alias ds='d status'
 alias da='d add'
@@ -10,6 +14,8 @@ alias di='d diff'
 alias dsa='d submodule add'
 alias dss='d submodule status'
 alias sp="sudo system76-power"
+alias wgu="wg-quick up Laptop"
+alias wgd="wg-quick down Laptop"
 
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
@@ -24,4 +30,11 @@ function dsr() {
 	d submodule deinit -f "$HOME/.zsh/$1"
   rm -rf "$HOME/.dotfilez/modules/$1"
 	d rm -f "$HOME/.zsh/$1"
+	sed -i '/source $HOME\/.zsh\/'"$1"'/d' $HOME/.zshrc
 }
+
+# Avoid overwriting of keybinds in autoexecuted function by zsh-vi-mode
+function zvm_after_init() {
+    source $HOME/.zsh/fzf-zsh-plugin/fzf-zsh-plugin.plugin.zsh
+}
+
