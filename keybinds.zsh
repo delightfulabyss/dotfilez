02fc18b68942bfb46c8aa916dfb503676f353954
Matthew Watman <mattwatman@gmail.com>
2024-08-15 22:00:22 -0400

    add zsh keybinds and update nvim plugins

---
+++ keybinds.zsh
@@ -0,0 +1,9 @@
+
+# autosuggestions
+bindkey '^A' autosuggest-accept
+bindkey '^ ' autosuggest-execute
+bindkey '^M ' autosuggest-clear
+bindkey '^T' autosuggest-toggle
+bindkey '^F' forward-word
+bindkey '^B' backward-word
+
